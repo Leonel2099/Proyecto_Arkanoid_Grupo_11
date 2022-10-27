e54153f0c71a83755914f57aeba03a99060d866d
arielsagrero <nasagrero2012@gmail.com>
2022-10-27 05:50:46 -0300

    correcciones realizadas al componente LevelOne: agregado de efecto de particulas a la pelota, transicion de escenas dependiendo si el jugador gana o pierde y modificacion del rebote de la pelota de acuerdo al lugar de impacto con el player. Ariel Sagrero

--- src/components/LevelOne.js
+++ src/components/LevelOne.js
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-export default class Game extends Phaser.Scene {
+export default class LevelOne extends Phaser.Scene {
     constructor(config) {
         super('LevelOne')
         this.config = config
@@ -12,8 +12,11 @@ export default class Game extends Phaser.Scene {
         this.gameOverText = null;
         this.winText = null;
         //this.sonido = null;
-        this.velocidadInicial = -300;
+        this.velocidadInicialY = -300;
+        this.velocidadInicialX = -72;
         this.barrasEliminadas = 0;
+        this.particles=null
+        this.emitter=null
     }
     create() {
         //colision entre paredes del canva 
@@ -26,21 +29,22 @@ export default class Game extends Phaser.Scene {
         this.createBall();
         //Creacion de Bloque
         this.createBlocks();
+        //Creacion de particulas
+        this.createParticles();
         //se crea el objeto cursors para determinar si una tecla es pulsada o no
         this.cursors = this.input.keyboard.createCursorKeys();
         //this.physics.add.collider(this.pelota, this.player, this.bar, null, this);
-        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
-        
+        this.scoreText = this.add.text(20, 30, 'Score: 0', { fontSize: '32px', fill: '#fff' });
     }
     update() {
         //movimiento del player segun se presione la tecla izquierda o derecha
-        if (this.cursors.left.isDown && this.player.x >= 120) {
+        if (this.cursors.left.isDown && this.player.x >= 80) {
             this.player.setVelocityX(-500);
             if (this.pelota.getData('startingPoint')) {
                 this.pelota.setVelocityX(-500);
             }
         }
-        else if (this.cursors.right.isDown && this.player.x <= 680) {
+        else if (this.cursors.right.isDown && this.player.x <= 555) {
             this.player.setVelocityX(500);
             if (this.pelota.getData('startingPoint')) {
                 this.pelota.setVelocityX(500)
@@ -54,17 +58,25 @@ export default class Game extends Phaser.Scene {
         }
         //Lanzamiento de pelota con la tecla 'Space'
         if (this.cursors.space.isDown && this.pelota.getData('startingPoint')) {
-            //this.pelota.setVelocity(-75,-300)
-            this.pelota.setVelocity(-72, this.velocidadInicial)
+            this.pelota.setVelocity(this.velocidadInicialX, this.velocidadInicialY)
             this.pelota.setData('startingPoint', false)
+            this.emitter.startFollow(this.pelota);
         }
 
         //alerta de salida del canvas
         if (this.pelota.y > 615) {
             this.pelota.disableBody(true, true); // la pelota al pasar el limite superior del canvas se elimina
-            this.gameOverText = this.add.text(300, 300, 'Game Over', { fontSize: '50px', fill: '#732C00' });
             this.physics.pause();
             this.player.setTint(0xff0000);
+            this.scene.start('GameOver')
+        }
+        if (this.pelota.y <= 45) {
+            this.pelota.setVelocityY(this.velocidadInicialY * (-1))
+        }
+        if (this.pelota.x <= 30) {
+            this.pelota.setVelocityX(this.velocidadInicialX * (-1))
+        } else if (this.pelota.x >= 610) {
+            this.pelota.setVelocityX(this.velocidadInicialX)
         }
 
     }
@@ -73,43 +85,73 @@ export default class Game extends Phaser.Scene {
     }
     createPlayer() {
         //se añade fisica al player
-        this.player = this.physics.add.image(400, 600, 'player').setImmovable().setScale(0.3);
+        this.player = this.physics.add.image(300, 600, 'player').setImmovable().setScale(0.3);
         //se quita la gravedad al player
         this.player.body.allowGravity = false;
         //evita que el player atraviese el borde izquierdo y derecho
         this.player.setCollideWorldBounds(true);
     }
     createBall() {
-        this.pelota = this.physics.add.image(400, 580, 'pelota').setScale(0.4);
+        this.pelota = this.physics.add.image(300, 576, 'pelota').setScale(0.4);
         //se agrega el rebote de la pelota con los bordes
         this.pelota.setCollideWorldBounds(true);
         this.pelota.setBounce(1);
         this.pelota.setData('startingPoint', true);
         //se agrega la colision entre la pelota y el player
-        this.physics.add.collider(this.pelota, this.player);
+        this.physics.add.collider(this.pelota, this.player, this.collisionPlayer, null, this);
+        
+    }
+    collisionPlayer(pelota, player) {
+        // Increase the velocity of the ball after it bounces
+        this.pelota.setVelocityY(this.velocidadInicialY - 5);
+
+        let newXVelocity = Math.abs(this.velocidadInicialX) + 5;
+        // If the ball is to the left of the player, ensure the X Velocity is negative
+        if (this.pelota.x < this.player.x) {
+            this.pelota.setVelocityX(-newXVelocity);
+        } else {
+            this.pelota.setVelocityX(newXVelocity);
+        }
     }
     createBlocks() {
         this.barras = this.physics.add.staticGroup();
         let barraDistanciaHorizontal = 80;
-        for (let i = 0; i < 7; i++) {
-            this.barras.create(barraDistanciaHorizontal, 100, 'barra');
+        for (let i = 0; i < 9; i++) {
+            //this.barras.create(barraDistanciaHorizontal, 100, 'barra');
             this.barras.create(barraDistanciaHorizontal, 135, 'barra');
-            this.barras.create(barraDistanciaHorizontal, 170, 'barra');
-    
-            barraDistanciaHorizontal += 80;
+            //this.barras.create(barraDistanciaHorizontal, 170, 'barra');
+            barraDistanciaHorizontal += 60;
         }
         //se agrega la colision entre la pelota y las barras
         this.physics.add.collider(this.pelota, this.barras, this.collectBarra, null, this);
+    }
+    createParticles(){
+        this.particles = this.add.particles('blue');
+        this.emitter = this.particles.createEmitter({
+            speed: 10,
+            scale: { start: 0.2, end: 0 },
+            frequency:10,
+            blendMode: 'ADD'
+        });
     }
 
     //velocidadInicial = 50;
     collectBarra(pelota, barras) {
         barras.disableBody(true, true);
+        // if (this.pelota.body.velocity.x == this.velocidadInicialX) {
+        //     let randNum = Math.random();
+        //     console.log(randNum)
+        //     if (randNum >= 0.5) {
+        //         this.pelota.setVelocityX(150);
+        //     } else {
+        //         this.pelota.setVelocityX(-150);
+        //     }
+        // }
         this.score += 10;
         this.scoreText.setText('Score: ' + this.score);
-        if (this.score === 300) {
+        if (this.score === 90) {
             this.physics.pause();
-            this.winText = this.add.text(300, 300, 'YOU WON', { fontSize: '50px', fill: '#00B806' });
+            this.scene.start('LevelTwo')
         }
     }
 };
